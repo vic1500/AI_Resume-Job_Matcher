723e07bbe22c4db99ae8d2b3148b6c64f07fe63e
victor <victordman15@gmail.com>
2026-02-21 09:42:54 +0100

    Added import.meta.env.VITE_API_URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: '/choreo-apis/ai-resume-job-matcher/backend/v1/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || '/choreo-apis/ai-resume-job-matcher/backend/v1/api/v1',
 });
 
 // 2. Add an "Interceptor" to automatically add the Token
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -18,6 +18,7 @@ export default function Login() {
 
     } catch (err) {
       setError("Login failed");
+      console.error(err)
     }
   };
 
